6cc652825dd6794d35c383f145b639d262d37992
Uzair Nasir <uxairjan3@gmail.com>
2024-07-08 20:49:11 +0500

    js variables

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-var email, name, age, gender, firstName, lastName; 
+var email, name, age, gender, firstName, lastName; // string, integer, float, character
+// varible name should be start with small character
+// varible name should be a meningful name
+// varible name can be start with underscore _ 
+// varible name should be camel case or snack case
+// varible name should not include any other special character
+// varible name should NOT START WITH NUMBER OR ANY SPECIAL CHARACTER
 
+email="mannan@gmail.com"
+name="irtiza"
+age=17
+gender='M'
+firstName="irtiza"
+lastName="hussain"
 
-console.log(greetings+ " 5")
-console.log(greetings+" irtiza")
-console.log(greetings+" uzair")
-console.log(greetings+" majid")
+console.log(email, name, age, gender, firstName, lastName)
+
+console.log(email, firstName)
+console.log(email, lastName)
+console.log(email, gender)
+
